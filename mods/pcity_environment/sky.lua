3b735d336b89a65cbfe478091305629c51e0fbc8
Jan Wielkiewicz <tona_kosmicznego_smiecia@interia.pl>
2024-03-19 23:14:43 +0100

    Environment: Sky: Rework code, add more textures.

--- mods/pcity_environment/sky.lua
+++ mods/pcity_environment/sky.lua
@@ -1,6 +1,6 @@
 --[[
     This is a part of "Perfect City".
-    Copyright (C) 2023 Jan Wielkiewicz <tona_kosmicznego_smiecia@interia.pl>
+    Copyright (C) 2023, 2024 Jan Wielkiewicz <tona_kosmicznego_smiecia@interia.pl>
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -21,56 +21,106 @@ local math = math
 
 ---------------------------------------------------
 -- In this section only texture operations happen
+---------------------------------------------------
+
+-- General texture operations
 
 local function tx_name(name)
     return mod_name.."_"..name..".png"
 end
 
+local function overlay(textures, new_textures)
+    if type(new_textures) == "string" then
+        for side, texture in pairs(textures) do
+            textures[side] = texture.."^"..new_textures
+        end
+    elseif type(new_textures) == "table" then
+        for side, new_texture in pairs(new_textures) do
+            textures[side] = textures[side].."^"..new_textures[side]
+        end
+    end
+end
+
+local function overlay_horizontal(textures, new_texture)
+    local horizontal = {
+        front = new_texture,
+        back = new_texture,
+        left = new_texture,
+        right = new_texture,
+    }
+    overlay(textures, horizontal)
+end
+
+local function textuers_to_mt_format(textures)
+    local new_textures = {}
+    new_textures[1] = textures.up
+    new_textures[2] = textures.down
+    new_textures[3] = textures.front
+    new_textures[4] = textures.back
+    new_textures[5] = textures.left
+    new_textures[6] = textures.right
+    return new_textures
+end
+
+-- Perfect City specific textures and functions
+
+local base_gray_tx = tx_name("base")
+local base_black_tx = tx_name("base_black")
+local base_blue_tx = tx_name("base_blue")
 local down_tx = tx_name("down")
 local noise_tx = tx_name("noise")
+local horizon_tx = tx_name("horizon")
 local city_tx = tx_name("city")
 local chimneys_tx = tx_name("chimneys_left")
-local cranes_front_tx = tx_name("cranes_front")
-local cranes_back_tx = tx_name("cranes_back")
-local cranes_left_tx = tx_name("cranes_left")
-local cranes_right_tx = tx_name("cranes_right")
+local cranes = {
+    front = tx_name("cranes_front"),
+    back = tx_name("cranes_back"),
+    left = tx_name("cranes_left"),
+    right = tx_name("cranes_right"),
+}
+local power_lines_tx = tx_name("power_lines")
+local silos_tx = tx_name("silos")
 local smog_tx = tx_name("smog")
 
-local function sky_noise_textures()
+local function sky_base_textures(base_tx)
     local textures = {
-        up = noise_tx,
+        up = base_tx,
         down = down_tx,
-        front = noise_tx,
-        back = noise_tx,
-        left = noise_tx,
-        right = noise_tx,
+        front = base_tx,
+        back = base_tx,
+        left = base_tx,
+        right = base_tx,
     }
     return textures
 end
 
+local function overlay_horizon(textures)
+    overlay_horizontal(textures, horizon_tx)
+end
+
 local function overlay_city(textures)
-    textures.front = textures.front.."^"..city_tx
-    textures.back = textures.back.."^"..city_tx
-    textures.left = textures.left.."^"..city_tx
-    textures.right = textures.right.."^"..city_tx
+    overlay_horizontal(textures, city_tx)
 end
 
 local function overlay_cranes(textures)
-    textures.front = textures.front.."^"..cranes_front_tx
-    textures.back = textures.back.."^"..cranes_back_tx
-    textures.left = textures.left.."^"..cranes_left_tx
-    textures.right = textures.right.."^"..cranes_right_tx
+    overlay(textures, cranes)
+end
+
+local function overlay_power_lines(textures)
+    -- textures.right = textures.right.."^"..power_lines_tx
+    -- textures.right = textures.right.."^"..silos_tx
 end
 
 local function overlay_chimneys(textures)
-    textures.left = textures.left.."^"..chimneys_tx
+    overlay(textures, {left = chimneys_tx})
 end
 
 local function overlay_smog(textures)
-    textures.front = textures.front.."^"..smog_tx
-    textures.back = textures.back.."^"..smog_tx
-    textures.left = textures.left.."^"..smog_tx
-    textures.right = textures.right.."^"..smog_tx
+    overlay_horizontal(textures, smog_tx)
+end
+
+local function overlay_noise(textures)
+    overlay(textures, noise_tx)
 end
 
 local monster_texture = tx_name("crane_monster")
@@ -92,25 +142,21 @@ end
 
 -- Ready to use sky textures translated into the minetest format
 local function default_textures()
-    local raw_textures = sky_noise_textures()
+    local raw_textures = sky_base_textures(base_gray_tx)
+    overlay_horizon(raw_textures)
     overlay_city(raw_textures)
     overlay_chimneys(raw_textures)
     overlay_cranes(raw_textures)
+    overlay_power_lines(raw_textures)
     overlay_monster(raw_textures)
+    overlay_noise(raw_textures)
     overlay_smog(raw_textures)
-    local textures = {}
-    textures[1] = raw_textures.up
-    textures[2] = raw_textures.down
-    textures[3] = raw_textures.front
-    textures[4] = raw_textures.back
-    textures[5] = raw_textures.left
-    textures[6] = raw_textures.right
-
-    return textures
+    return textuers_to_mt_format(raw_textures)
 end
 
 ---------------------------------------------------
 -- In this section sky operations happen
+---------------------------------------------------
 
 local clouds = {
     density = 0.4,
