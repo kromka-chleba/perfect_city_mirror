fbcad99806f6ca6d385e25eb3cae0acdfb3c3017
Jan Wielkiewicz <tona_kosmicznego_smiecia@interia.pl>
2024-07-04 23:30:12 +0200

    Mapchunk Shepherd: Fix licensing.

--- mods/mapchunk_shepherd/chunk_utils.lua
+++ mods/mapchunk_shepherd/chunk_utils.lua
@@ -1,3 +1,21 @@
+--[[
+    This is a part of "Perfect City".
+    Copyright (C) 2023 Jan Wielkiewicz <tona_kosmicznego_smiecia@interia.pl>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+--]]
+
 -- Mapchunk Shepherd
 -- License: GNU GPLv3
 -- Copyright © Jan Wielkiewicz 2023
