940d33ec32e54652a9fce5c7b18ea4c91ed06eed
Jan Wielkiewicz <tona_kosmicznego_smiecia@interia.pl>
2024-11-11 14:19:59 +0100

    Characters: Add Alex model.

---
+++ mods/pcity_characters/README.md
@@ -0,0 +1,10 @@
+# Licenses for textures, models and sounds
+
+## Copyright © 2024 Jan Wielkiewicz
+  CC-BY-SA 4.0 https://creativecommons.org/licenses/by-sa/4.0/
+
+* every texture, model, sound unless stated otherwise
+
+## Not covered by copyright?
+
+* pcity\_characters\_palette.png (a composite of GIMP palettes)
---
+++ mods/pcity_characters/alex.lua
@@ -0,0 +1,21 @@
+--[[
+    This is a part of "Perfect City".
+    Copyright (C) 2024 Jan Wielkiewicz <tona_kosmicznego_smiecia@interia.pl>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+--]]
+
+local pcc = pcity_characters
+local mod_name = minetest.get_current_modname()
+local mod_path = minetest.get_modpath("pcity_characters")
